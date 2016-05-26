8e75df881f2292560e3aaa01e01b0878b684ea43
mwendwa5 <mwendwa5@gmail.com>
2016-05-26 14:32:00 +0300

    Add files via upload

--- sms/sendgroup.php
+++ sms/sendgroup.php
@@ -4,16 +4,28 @@ if (!$conn) {
     die('Could not connect to database ' . mysql_error());
  }
 mysql_select_db('churchcrm');
-$query = "SELECT * FROM list_lst WHERE lst_ID = 1 ORDER BY lst_OptionSequence";
+$query = "SELECT * FROM group_grp ORDER BY grp_ID";
 $result = mysql_query($query);
 if(!$result) die(mysql_error());
+$send = $_GET['send'];
+$group = $_GET['group'];
+if(isset($group) and (isset($send))){
+$query2 = "select * from group_grp where grp_ID='$group'";
+$result2 = mysql_query($query2);
+if (!$result2) die(mysql_error());
+while ($row = mysql_fetch_array($result2)) 
+$grpname = $row['grp_Name'];
+echo "Messages have been send successfully to the group ".$grpname;
+}
+//echo $send;
+//echo $group;
 ?>
 <FORM action=sendgrpsms.php METHOD=POST>
 <h1>Sending Group Message</h1>
 Select group to send message to: <br>
-<?php echo "<select name='groupname'>";
+<?php echo "<select name='grp_ID'>";
 while ($row = mysql_fetch_array($result)) {
-    echo "<option value='" . $row['lst_OptionName'] ."'>" . $row['lst_OptionName'] ."</option>";
+    echo "<option value='" . $row['grp_ID'] ."'>" . $row['grp_Name'] ."</option>";
 }
 echo "</select>";
 ?>
